8e17af28add73693510ff35d11bcff2a75c6072c
d1729 <debarshi1729@gmail.com>
2017-07-01 01:02:54 +0530

    Better code

--- Code/C++/Array/sortByFrequency.cpp
+++ Code/C++/Array/sortByFrequency.cpp
@@ -1,45 +1,43 @@
 #include <iostream>
 #include <map>
+#include <utility>
 #include <algorithm>
 #include <vector>
-#include <utility>
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-    int arr[] = {2, 5, 2, 8, 5, 6, 8, 8};
-    int n = sizeof(arr) / sizeof(int);
-    /*cin>>n;
-    arr = new int[n];
-    for(int i = 0; i < n; ++i)
-        cin>>arr[i];
-    */
-    map<int, pair<int, int>> m;
+void sortByFrequency(int arr[], int n){
+    map<int, int>m;
+    vector<pair<int, int>>v;
     for(int i = 0; i < n; ++i){
-        if(m.find(arr[i]) != m.end())
-            ++m[arr[i]].first;
-        else
-            m.insert(make_pair(arr[i], make_pair(1, i)));
+        if(m.find(arr[i]) != m.end())m[arr[i]]++;
+        else m[arr[i]] = 1;
     }
-    vector<pair<int, pair<int, int>>>v;
-    map<int, pair<int, int> >::iterator it;
+    map<int, int>::iterator it;
     for(it = m.begin(); it != m.end(); ++it)
         v.push_back(make_pair(it->first, it->second));
-    sort(v.begin(), v.end(), [](pair<int, pair<int, int>>&p1, pair<int, pair<int, int>>&p2){
-        if(p1.second.first == p2.second.first)
-            return p1.second.second < p2.second.second;
-        return p1.second.first > p2.second.first;
+    sort(v.begin(), v.end(), [](pair<int, int>& p1, pair<int, int>& p2){
+        if(p1.second == p2.second)
+            return p1.first < p2.first;
+        return p1.second > p2.second;
     });
-    int i = 0;
-    for(int j = 0; j < v.size(); ++j){
-        int sz = v[j].second.first;
-        while (sz > 0) {
-            arr[i] = v[j].first;
-            --sz;
-            ++i;
-        }
-    }
-    for(i = 0; i < n; ++i)
+    int j = 0;
+    for(auto it = v.begin(); it != v.end(); ++it)
+        for(int i = 0; i < it->second; ++i)
+            arr[j++] = it->first;
+    for(int i = 0; i < n; ++i)
         cout<<arr[i]<<" ";
     cout<<"\n";
-    return 0;
+}
+
+int main() {
+	int t, n, *arr;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    arr = new int[n];
+	    for(int i = 0; i < n; ++i)cin>>arr[i];
+        sortByFrequency(arr, n);
+	    delete []arr;
+	}
+	return 0;
 }
